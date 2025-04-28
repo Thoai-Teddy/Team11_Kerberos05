4a938e4f466ecbd5cd059d2d1f4b13619c1623ec
Hanh Thao <dohanhthao2004@gmail.com>
2025-04-28 15:59:30 +0700

    add AES-CBC

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -1,12 +1,11 @@
 ﻿#pragma once
+
+#define CRT_SECURE_NO_WARNINGS
+
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
 #include <csignal> // Dùng signal() để bắt Ctrl+C
-//#include <openssl/aes.h>      // Cho các hàm, cấu trúc AES
-//#include <openssl/evp.h>      // Cho API mã hóa cấp cao (EVP_* functions)
-//#include <openssl/rand.h>     // Để tạo số ngẫu nhiên (ví dụ tạo IV)
-//#include <openssl/err.h>      // Để xử lý lỗi OpenSSL
 #include <vector>
 #include <stdexcept>
 #include <iomanip>
@@ -14,17 +13,39 @@
 #include <cstring>
 #include <cstdint>
 
-
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
-
-//client
-//std::vector<unsigned char> aes_encrypt_cbc(const unsigned char* plaintext, int plaintext_len, const unsigned char* key, const unsigned char* iv);
-void handleCtrlC(int sig);
 
 //hash SHA1
 uint32_t left_rotate(uint32_t value, unsigned int count);
 string sha1(const string& input);
 
+// Hàm chuyển byte thành chuỗi hexadecimal
+string bytesToHex(const vector<unsigned char>& bytes);
+
+//AES-CBC:
+unsigned int bytesToWord(const unsigned char* bytes);
+void wordToBytes(unsigned int word, unsigned char* bytes);
+void SubBytes(unsigned char* state);
+void ShiftRows(unsigned char* state);
+unsigned char GF(unsigned char x);
+void MixColumns(unsigned char* state);
+void AddRoundKey(unsigned char* state, const unsigned char* roundKey);
+void KeyExpansion(const unsigned char* key, unsigned char* roundKeys);
+void aes_encrypt_block(unsigned char* block, const unsigned char* key);
+void xor_blocks(unsigned char* dst, const unsigned char* src);
+void padding(vector<unsigned char>& data);
+vector<unsigned char> aes_cbc_encrypt(const vector<unsigned char>& plaintext, const vector<unsigned char>& key, const vector<unsigned char>& iv);
+void InvSubBytes(unsigned char* state);
+void InvShiftRows(unsigned char* state);
+void InvMixColumns(unsigned char* state);
+void unpadding(vector<unsigned char>& data);
+void aes_decrypt_block(unsigned char* block, const unsigned char* key);
+vector<unsigned char> aes_cbc_decrypt(const vector<unsigned char>& ciphertext, const vector<unsigned char>& key, const vector<unsigned char>& iv);
+
+vector<unsigned char> padString(const string& input);
+string unpadString(const vector<unsigned char>& input);
+
+//Step 6: Service Server reply to Client:
 
